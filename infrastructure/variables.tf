e3b1c446dc84e8330cf6fd9cb2259d83a2f28d0b
thobela <thobela.sixpence@gmail.com>
2026-09-05 22:24:00 +0200

    feat(infrastructure): add variables.tf for AWS deployment configuration

---
+++ infrastructure/variables.tf
@@ -0,0 +1,7 @@
+"""Terraform variables for AWS deployment."""
+# TODO: Create variables.tf with:
+# - instance_type (default: t2.micro)
+# - availability_zone
+# - s3_bucket_name
+# - ecs_cluster_name
+# - docker_image_tag
